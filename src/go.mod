da6bebf3ec546037916419c090e4066bcbbd8dbe
Salma WADOUACHI <99723124+TerraNovaX@users.noreply.github.com>
2024-02-10 22:38:55 +0100

    feat: token securityManager router (#1)
    * feat: token securityManager router
    
    * correction SalmaPR
    
    ---------
    
    Co-authored-by: meliocus <lucasvanessche@gmail.com>

--- src/go.mod
+++ src/go.mod
@@ -4,5 +4,6 @@ go 1.21.6
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1
-
+	github.com/go-chi/chi/v5 v5.0.11 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 )
---
+++ src/managers/tokenManager.go
@@ -0,0 +1,21 @@
+package Managers
+
+import (
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+func createToken(email string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"email": email,
+			"exp":   time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
