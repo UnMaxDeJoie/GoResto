226a476f2d90550db03a600c086fa1bc963907ff
UnMaxDeJoie <maxait93@gmail.com>
2024-02-11 20:57:20 +0100

    feat/mailManager: envoi de mail

--- src/managers/mailManager.go
+++ src/managers/mailManager.go
@@ -10,13 +10,13 @@ import (
 func SendMailHandler(w http.ResponseWriter, r *http.Request) {
 	// Créer un e-mail
 	e := email.NewEmail()
-	e.From = "Votre nom <testmaxgo@gmail.com>"
+	e.From = "Votre nom <goresto833@gmail.com>"
 	e.To = []string{"maxait93@gmail.com"}
 	e.Subject = "Test d'envoi d'e-mail"
 	e.Text = []byte("Ceci est un test d'envoi d'e-mail en utilisant GoMail.")
 
 	// Authentification SMTP
-	auth := smtp.PlainAuth("testmaxgo", "testmaxgo@gmail.com", "!gomaxTest1", "smtp.gmail.com")
+	auth := smtp.PlainAuth("testmaxgo", "goresto833@gmail.com", "Goresto23!!", "smtp.gmail.com")
 
 	// Envoi de l'e-mail
 	err := e.Send("smtp.gmail.com:587", auth)
